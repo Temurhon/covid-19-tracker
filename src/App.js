7ba2dd21d85495503e66418c0b93e1b4e206e786
Temurhon Kamariddinov <temurhon.dev@gmail.com>
2020-09-01 16:06:51 +0100

    .

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Cards from './components/Cards/Cards';
 import Charts from './components/Charts/Charts';
 import CountryPicker from './components/CountryPicker/CountryPicker';
 
+import styles from './App.module.css';
+
 class App extends React.Component{
     render(){
         return(
-            <div>
+            <div className={styles.container}>
             <Cards/>
             <CountryPicker/>
             <Charts/>
---
+++ src/api/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+const fetchData = async = () => {
+    //try will be executed if the fetch is successful, otherise it'll be catch.
+try{
+    //get response from await.
+const response = await.axios.get(url);
+//to see what we are getting.
+console.log(response);
+}
+catch (error){
+
+}
+}
