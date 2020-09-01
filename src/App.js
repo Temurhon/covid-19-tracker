60c96dfcbd537b25b1e1eb20402e4f4e0d5bdbb8
Temurhon Kamariddinov <temurhon.dev@gmail.com>
2020-09-01 16:15:45 +0100

    added api

--- src/App.js
+++ src/App.js
@@ -5,9 +5,20 @@ import Cards from './components/Cards/Cards';
 import Charts from './components/Charts/Charts';
 import CountryPicker from './components/CountryPicker/CountryPicker';
 
+//change the styling of the webapp
 import styles from './App.module.css';
 
+//import the api index file to display it on webapp
+import {fetchData} from './api';
+
 class App extends React.Component{
+
+    async componentDidMount(){
+        const data = await fetchData();
+        console.log(data);
+    }
+
+
     render(){
         return(
             <div className={styles.container}>
--- src/api/index.js
+++ src/api/index.js
@@ -2,13 +2,17 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-const fetchData = async = () => {
+export const fetchData = async () => {
     //try will be executed if the fetch is successful, otherise it'll be catch.
 try{
     //get response from await.
-const response = await.axios.get(url);
+const response = await axios.get(url);
+
 //to see what we are getting.
 console.log(response);
+
+return response;
+
 }
 catch (error){
 
