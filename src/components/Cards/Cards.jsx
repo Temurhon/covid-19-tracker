6aeeab5df164717ecb02efd48319753a70c24d95
Temurhon Kamariddinov <temurhon.dev@gmail.com>
2020-09-01 16:57:36 +0100

    added first data

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -6,10 +6,15 @@ import {Card, CardContent, Typography, Grid} from '@material-ui/core';
 //importing styles for cards
 import styles from './Cards.module.css';
 
-const Cards = (props) => {
-    console.log(props);
+//dependency to count up so it has live chaing effect on the webapp
 
-    
+
+const Cards = ({ data : {confirmed, deaths, recovered, lastUpdate} }) => {
+
+//without if statements, the data will not be shown and will through an error
+    if(!confirmed){
+        return 'Loading Data....'
+    }
 
     return (
         <div className={styles.container}>
@@ -17,9 +22,51 @@ const Cards = (props) => {
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary">
-
+                        Infected
                         </Typography>
+                        <Typography variant="h5">
+                        {confirmed.value}
+                        </Typography>
+                        <Typography color="textSecondary">
+                        Real Date
+                        </Typography>
+                        <Typography variant="body2">
+                        Number of active cases of COVID-19
+                        </Typography>
+                    </CardContent>
 
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary">
+                        Recovered
+                        </Typography>
+                        <Typography variant="h5">
+                        Real Data
+                        </Typography>
+                        <Typography color="textSecondary">
+                        Real Date
+                        </Typography>
+                        <Typography variant="body2">
+                        Number of recoveries of COVID-19
+                        </Typography>
+                    </CardContent>
+
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary">
+                        Deaths
+                        </Typography>
+                        <Typography variant="h5">
+                        Real Data
+                        </Typography>
+                        <Typography color="textSecondary">
+                        Real Date
+                        </Typography>
+                        <Typography variant="body2">
+                        Number of death cases of COVID-19
+                        </Typography>
                     </CardContent>
 
                 </Grid>
