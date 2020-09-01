aeed0b37900fba2192de87a0b9069927f514c742
Temurhon Kamariddinov <temurhon.dev@gmail.com>
2020-09-01 16:41:17 +0100

    added component styles

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
 
-const Cards = () => {
+//import card visuals from material ui
+import {Card, CardContent, Typography, Grid} from '@material-ui/core';
+
+//importing styles for cards
+import styles from './Cards.module.css';
+
+const Cards = (props) => {
+    console.log(props);
+
+    
+
     return (
-        <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spacing = {3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary">
+
+                        </Typography>
+
+                    </CardContent>
+
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
